daad7b9557c6a78c03a6041ccc41cecf5dcebfa3
Brandon Bremen <bbremen@gmail.com>
2017-10-23 21:33:24 -0700

    keyboard input and ship state

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,43 @@ extern crate piston_window;
 
 use piston_window::*;
 
-// Implementing a foreign trait for a foreign type not allowed by Rust:
-//impl std::ops::Mul for piston_window::math::Matrix2d {
-//    // The multiplication of rational numbers is a closed operation.
-//    type Output = Self;
-//
-//    fn mul(self, rhs: Self) -> Self {
-//        return math::multiply(self, rhs);
-//    }
-//}
+#[allow(dead_code)]
+pub struct Ship {
+    // motion
+    position: math::Vec2d,
+    rotation: f64,
+    velocity: math::Vec2d,
+
+    // controls
+    thrusters_on: bool,
+    rotating_left: bool,
+    rotating_right: bool,
+}
+
+impl Default for Ship {
+    fn default() -> Ship {
+        Ship {
+            position: [0.0, 0.0],
+            rotation: 0.0,
+            velocity: [0.0, 0.0],
+            thrusters_on: false,
+            rotating_left: false,
+            rotating_right: false,
+        }
+    }
+}
+
+impl Ship {
+    fn rotation_speed(&self) -> f64 { 2.0 }
+    fn radius(&self) -> f64 { 20.0 }
+}
+
+pub struct Player {
+    ship: Ship
+}
 
 pub struct App {
-    rotation: f64   // Rotation for the square.
+    player: Player
 }
 
 impl App {
@@ -21,7 +46,7 @@ impl App {
         window.draw_2d(event, |context, graphics| {
             clear([1.0; 4], graphics);
 
-            let rect_size = [100.0, 100.0];
+            let rect_size = [self.player.ship.radius() * 2.0, self.player.ship.radius() * 2.0];
             let rect_midpoint = [rect_size[0] / 2.0, rect_size[1] / 2.0];
 
             use math::*;
@@ -32,7 +57,7 @@ impl App {
                 args.height as f64 / 2.0 - rect_midpoint[1]]));
             // rotate around the midpoint of the square
             let t = multiply(t, translate(rect_midpoint));
-            let t = multiply(t, rotate_radians(self.rotation));
+            let t = multiply(t, rotate_radians(self.player.ship.rotation));
             let t = multiply(t, translate(math::mul_scalar(rect_midpoint, -1.0)));
 
             rectangle([1.0, 0.0, 0.0, 1.0], // red
@@ -44,20 +69,35 @@ impl App {
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
+        let player_ship = &mut self.player.ship;
+
+        if player_ship.rotating_left {
+            player_ship.rotation -= player_ship.rotation_speed() * args.dt;
+        }
+
+        if player_ship.rotating_right {
+            player_ship.rotation += player_ship.rotation_speed() * args.dt;
+        }
+    }
+
+    fn key_down(&mut self, state: ButtonState, key: Key) {
+        match key {
+            Key::Up => self.player.ship.thrusters_on = state == ButtonState::Press,
+            Key::Left => self.player.ship.rotating_left = state == ButtonState::Press,
+            Key::Right => self.player.ship.rotating_right = state == ButtonState::Press,
+            _ => println!("{:?}ed keyboard key '{:?}'", state, key)
+        }
     }
 }
 
 fn main() {
     let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
+        WindowSettings::new("Tekuno", [640, 480])
             .exit_on_esc(true).build().unwrap();
 
 
-    // Create a new game and run it.
     let mut app = App {
-        rotation: 0.0
+        player: Player { ship: Default::default() }
     };
 
     while let Some(event) = window.next() {
@@ -67,7 +107,14 @@ fn main() {
         }
 
         if let Some(u) = event.update_args() {
-            app.update(&u);
+            app.update(&&u);
         }
+
+        if let Some(args) = event.button_args() {
+            match args.button {
+                Button::Keyboard(key) => app.key_down(args.state, key),
+                _ => {}
+            }
+        };
     }
 }
